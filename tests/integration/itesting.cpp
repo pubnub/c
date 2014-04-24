0fef8cc0127a30746a3b95007381d32d3303e89a
avy <alex7y@gmail.com>
2014-04-24 17:47:02 +0400

    tests: Add integration server test

--- tests/integration/itesting.cpp
+++ tests/integration/itesting.cpp
@@ -1,8 +1,15 @@
 
 #include "itesting.h"
+#ifdef _WIN32
+#include "windows.h"
+#endif
 
 int main(int argc, char* argv[])
 {
+#ifdef _WIN32
+	WSADATA WSAData;
+	WSAStartup(0x101, &WSAData);
+#endif
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
 }
